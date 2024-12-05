5a42a29136fa4441990e031a8329cbea06c68d80
Gowrishankar M N <142252255+gowrishankar-14@users.noreply.github.com>
2024-12-05 11:01:19 +0530

    Final Program Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,7 @@
-@Override
-public void perform(Run<?, ?> run, FilePath workspace, Launcher launcher, TaskListener listener)
-     PrintStream outOriginal = System.out;
-     PrintStream errOriginal = System.err;
-     try {
-        System.setOut(listener.getLogger());
-        System.setErr(listener.getLogger());
-        System.out.println("Testing System.out");
-        // Call your code that writes to System.out by default
-     } finally {
-        System.setOut(outOriginal);
-        System.setErr(errOriginal);
-     }
+public class Main
+{
+public static void main(String[] args)
+{
+System.out.println("Continuous Integration Using Jenkins");
+}
 }
